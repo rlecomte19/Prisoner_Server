9a694aed3595e8c263ba65781515e5f4d37f5cd6
RORO <romanlecomte@gmail.com>
2021-12-26 15:58:05 +0100

    [test] Testing main

--- src/main.c
+++ src/main.c
@@ -1,50 +1,40 @@
 #include "./Game/game.h"
 #include "./Communication/net_prisoner_server.h"
-#include "Configuration/configuration.h"
 #include "Results/results.h"
+#include "Configuration/configuration.h"
+
 
 int main()
 {
 	// Config à pas bouger atm
 	init_configuration();
 
-	GameList gl;
-	gl.gameList = (Game *)malloc(sizeof(Game) * 3);
-	gl.size = 3;
-	for (int i = 0; i < 3; i++)
-	{
-		const int RoundSize = 10;
+	init_server();
+	
+	// while(1){
+	// 	sleep(2);
+	// }
 
-		Game *g = &gl.gameList[i];
-		g->nbMaxRounds = RoundSize;
-		g->currentRound = RoundSize;
-		g->b = (Binome *)malloc(sizeof(Binome));
-		g->list_of_answers = (AnswerList *)malloc(sizeof(AnswerList));
+	// Binome *b = malloc(sizeof(Binome));
+	// b = _get_client_binome(3);
+	// printf("[BinomeList][Taile : %d] - \t\n [Binome 0] - \n [ReponseP1 : %d] \n [ReponseP2 : %d] \n [GameIndex : %d] \n [ID client 1 : %d] \n [ID client 2: %d]", 
+	// 	binome_config_list->size,
+	// 	binome_config_list->list[1].clients_answers->p1, 
+	// 	binome_config_list->list[1].clients_answers->p2, 
+	// 	binome_config_list->list[1].gameIndex,
+	// 	binome_config_list->list[1].clients_id[0],
+	// 	binome_config_list->list[1].clients_id[1]
+	// );
 
-		Binome *b = g->b;
-		b->clients_id[0] = rand() % 100;
-		b->clients_id[1] = rand() % 100;
-		b->gameIndex = i;
-
-		AnswerList *al = g->list_of_answers;
-		al->answers = (Answer *)malloc(sizeof(Answer) * RoundSize);
-		al->size = RoundSize;
-		for (int j = 0; j < RoundSize; j++)
-		{
-			if (rand() % 2 == 0)
-			{
-				al->answers[j].p1 = COLLAB;
-				al->answers[j].p2 = BETRAY;
-			}
-			else
-			{
-				al->answers[j].p1 = BETRAY;
-				al->answers[j].p2 = COLLAB;
-			}
-		}
-	}
-
-	store_results("rapport.csv", gl);
+	// printf("\n================================\n");
+ 
+ 	// printf("[BinomeTest] - \t\n [Binome 0] - \n [ReponseP1 : %d] \n [ReponseP2 : %d] \n [GameIndex : %d] \n [ID client 1 : %d] \n [ID client 2: %d]", 
+	// 	binome_config_list->list[2].clients_answers->p1, 
+	// 	binome_config_list->list[2].clients_answers->p1, 
+	// 	binome_config_list->list[2].gameIndex,
+	// 	binome_config_list->list[2].clients_id[0],
+	// 	binome_config_list->list[2].clients_id[1]
+	// );
 
 	return (0);
 }
