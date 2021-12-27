3617415d258aa424d350f99c74b6cade40f9ef6d
RORO <romanlecomte@gmail.com>
2021-12-27 04:31:41 +0100

    [test] Changing main to only use testing functions for further usage

--- src/main.c
+++ src/main.c
@@ -1,41 +1,13 @@
 #include "./Game/game.h"
 #include "./Communication/net_prisoner_server.h"
-#include "Results/results.h"
 #include "Configuration/configuration.h"
-
+#include "./Tests/test.h"
 
 int main()
 {
-	// Config à pas bouger atm
-	init_configuration();
-
-	init_server();
-
-	printf("TEST");
-	while(1){
-		sleep(1);
-	}
-
-	// Binome *b = malloc(sizeof(Binome));
-	// b = _get_client_binome(3);
-	// printf("[BinomeList][Taile : %d] - \t\n [Binome 0] - \n [ReponseP1 : %d] \n [ReponseP2 : %d] \n [GameIndex : %d] \n [ID client 1 : %d] \n [ID client 2: %d]", 
-	// 	binome_config_list->size,
-	// 	binome_config_list->list[1].clients_answers->p1, 
-	// 	binome_config_list->list[1].clients_answers->p2, 
-	// 	binome_config_list->list[1].gameIndex,
-	// 	binome_config_list->list[1].clients_id[0],
-	// 	binome_config_list->list[1].clients_id[1]
-	// );
-
-	// printf("\n================================\n");
+	test_initializations();
+	test_game_management();
+	test_connections();
  
- 	// printf("[BinomeTest] - \t\n [Binome 0] - \n [ReponseP1 : %d] \n [ReponseP2 : %d] \n [GameIndex : %d] \n [ID client 1 : %d] \n [ID client 2: %d]", 
-	// 	binome_config_list->list[2].clients_answers->p1, 
-	// 	binome_config_list->list[2].clients_answers->p1, 
-	// 	binome_config_list->list[2].gameIndex,
-	// 	binome_config_list->list[2].clients_id[0],
-	// 	binome_config_list->list[2].clients_id[1]
-	// );
-
 	return (0);
 }
